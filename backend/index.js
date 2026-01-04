6acd01c96ccfa34274f0181d2a45ca8b1802bc6e
RohanPatil <roborohan1@gmail.com>
2026-01-04 16:21:29 +0530

    3rd

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","https://library-management-system-five-alpha.vercel.app"],
     credentials: true,
   })
 );
